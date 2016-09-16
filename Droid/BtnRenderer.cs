ff113e25afc234c223c0c01917538ee3372a6d6f
Gerald Versluis <gerald@verslu.is>
2016-09-16 11:21:28 +0200

    Initial commit
    For StackOverflow question
    http://stackoverflow.com/questions/39525910/why-is-my-custom-renderer-no
    t-working

---
+++ Droid/BtnRenderer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using CustomRenderer.Droid;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer (typeof (Button), typeof (BtnRenderer))]
+namespace CustomRenderer.Droid
+{
+    public class BtnRenderer : ButtonRenderer
+    {
+        protected override void OnElementChanged (ElementChangedEventArgs<Xamarin.Forms.Button> e)
+        {
+            base.OnElementChanged (e);
+            var btn = this.Control as Android.Widget.Button;
+            btn.SetBackgroundColor (global::Android.Graphics.Color.Green);
+        }
+    }
+}
